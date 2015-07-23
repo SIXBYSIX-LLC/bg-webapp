71ad1bb9205d3425091038637a93f8a1e93b0523
Deepak P <deepakpatil84@gmail.com>
2015-07-24 01:13:49 +0530

    cart integration WIP

--- src/app/views/cart/cart.service.js
+++ src/app/views/cart/cart.service.js
@@ -4,7 +4,10 @@ angular.module('BG').factory('CartService',
     function (API,$http) {
     return {
       getCurrent:function(){
-        return $http.get(API.baseURL+"/carts/current");
+        return $http.get(API.baseURL+"carts/current");
+      },
+      addToCart:function(cartId,data){
+        return $http.post(API.baseURL+"carts/"+cartId+"/rentals",data)
       }
     }
   }
