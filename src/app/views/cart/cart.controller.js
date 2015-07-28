f98a35bb38eba0dee02a4eb42ca8b230b2f70120
Deepak P <deepakpatil84@gmail.com>
2015-07-29 00:30:05 +0530

    fixed cart update and remove..

--- src/app/views/cart/cart.controller.js
+++ src/app/views/cart/cart.controller.js
@@ -1,22 +1,33 @@
 angular.module('BG').controller('CartCtrl',
   /** @ngInject */
-    function ($scope,CartService) {
-      var currentCartId;
-      var mdl=$scope.mdl={};
-      CartService.getCurrent().then(function(response){
-        if(response && response.data && response.data.data){
-          currentCartId=response.data.data.id;
-          mdl.data= response.data.data;
+    function ($scope, CartService) {
+    var currentCartId;
+    var mdl = $scope.mdl = {};
+    function getCart(){
+      CartService.getCurrent().then(function (response) {
+        if (response && response.data && response.data.data) {
+          currentCartId = response.data.data.id;
+          mdl.data = response.data.data;
         }
       });
+    }
 
-      $scope.mdl.qtys=[0,1,2,3,4,5,6,7,8];
+    getCart();
 
-      $scope.mdl.shippingKinds={
-        "delivery":"Delivery",
-        "pickup":"Pickup"
-      };
+    $scope.mdl.qtys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    $scope.mdl.shippingKinds = {
+      "delivery": "Delivery",
+      "pickup": "Pickup"
+    };
+
+    $scope.remove = function (item) {
+      CartService.removeItem(currentCartId, item.id).then(getCart)
+    };
+
+    $scope.update = function (item) {
+      CartService.updateItem(currentCartId, item.id, item).then(getCart);
+    }
 
 
-
-    });
+  });
--- src/app/views/cart/cart.service.js
+++ src/app/views/cart/cart.service.js
@@ -8,6 +8,13 @@ angular.module('BG').factory('CartService',
       },
       addToCart:function(cartId,data){
         return $http.post(API.baseURL+"carts/"+cartId+"/rentals",data)
+      },
+      removeItem:function(cartId,id){
+        //https://api.marketplace.com/v1/carts/cart_id/
+        return $http.delete(API.baseURL+"carts/"+cartId+"/rentals/"+id);
+      },
+      updateItem:function(cartId,id,data){
+        return $http.patch(API.baseURL+"carts/"+cartId+"/rentals/"+id,data)
       }
     }
   }
