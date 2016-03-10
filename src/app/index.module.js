07361c8b51cb748f2d7de446460dabf3793ea481
hirenkesariya <hiren2hiren@gmail.com>
2016-03-10 12:50:47 +0530

    orders page search and sort

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -15,7 +15,8 @@
       'braintree-angular',
       'toggle-switch',
       'angular-ladda',
-      'chieffancypants.loadingBar'
+      'chieffancypants.loadingBar',
+      'daterangepicker'
     ])
     .run(["$rootScope",function($rootScope){
       $rootScope.dateFormat = 'dd-MMMM-yyyy';
--- src/app/views/seller-account/orders/list/list.controller.js
+++ src/app/views/seller-account/orders/list/list.controller.js
@@ -4,8 +4,19 @@ angular.module('BG').controller('ListSellerOrdersCtrl',
 
       var mdl = $scope.mdl = {};
 
+      $scope.searchOpen = false;
+      $scope.filterOpen = false;
+
       SellerOrdersService.getOrders().then(function (response) {
         mdl.orders = response.data.data;
       });
+
+      $scope.searchBar = function() {
+      	$scope.searchOpen = !$scope.searchOpen;
+      }
+
+      $scope.filterBar = function() {
+      	$scope.filterOpen = !$scope.filterOpen;
+      }
 
   });
