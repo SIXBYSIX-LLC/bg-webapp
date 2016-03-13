87dc778c037be6cc93447b181f3ad60dec28cbd4
asingh <amitksingh1490@gmail.com>
2016-03-13 09:30:08 +0530

    rented count

--- src/app/views/account/dashboard/dashboard.controller.js
+++ src/app/views/account/dashboard/dashboard.controller.js
@@ -1,6 +1,6 @@
 angular.module('BG').controller('DashboardCtrl',
   /** @ngInject */
-    function ($scope, JobsitesService,EquipmentsService) {
+    function ($scope, JobsitesService,EquipmentsService,OrdersService) {
     var mdl = $scope.mdl = {};
     mdl.jobSitesCount = 0;
     JobsitesService.getSitesCount($scope.user.id).then(function (count) {
@@ -8,6 +8,14 @@ angular.module('BG').controller('DashboardCtrl',
     });
     EquipmentsService.getFavoriteProductsCount($scope.user.id).then(function(count){
       mdl.favoriteCount=count;
+    });
+    OrdersService.getOrders().then(function(response){
+
+      mdl.rentedCount=0;
+      for(var i=0; i<response.data.data.length;i++){
+        mdl.rentedCount+=response.data.data[i].items.length;
+      }
+
     });
   }
 );
