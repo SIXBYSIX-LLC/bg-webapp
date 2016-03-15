b4acb10c29ac21e80fd3e12500d925436ecd6229
asingh <amitksingh1490@gmail.com>
2016-03-15 10:16:04 +0530

    buyer dashboard changes

--- src/app/scripts/filters/filter.js
+++ src/app/scripts/filters/filter.js
@@ -1,33 +1,22 @@
+
 angular.module('BG').filter('slice', function() {
   return function(arr, start, end) {
     return (arr || []).slice(start, end);
   };
-}).filter('daterange', function ()
-{
-  return function(conversations, start_date, end_date)
-  {
-    //console.log(conversations,start_date,end_date);
+}).filter("dateRangeFilter", function() {
+  return function(items, from, to) {
+   if(items)
+   {
+   var df = from ||0;
+    var dt = to||moment();
     var result = [];
-
-    // date filters
-    start_date = (start_date && !isNaN(Date.parse(start_date))) ? Date.parse(start_date) :  0;
-     end_date = (end_date && !isNaN(Date.parse(end_date))) ? Date.parse(end_date) : new Date().getTime();
-    console.log(conversations,start_date,end_date);
-    // if the conversations are loaded
-    if (conversations && conversations.length > 0)
-    {
-      angular.forEach(conversations, function (conversation,index)
-      {
-        console.log(conversation.date_created_at);
-        var conversationDate = new Date(conversation.date_created_at);
-        console.log(conversationDate);
-        if (conversationDate >= start_date && conversationDate <= end_date)
-        {
-          result.push(conversation);
-        }
-      });
-        console.log(result);
-      return result;
+    for (var i=0; i<items.length; i++){
+      var tf = moment(items[i].date_created_at);
+      console.log(tf);
+      if (tf > df && tf < dt)  {
+        result.push(items[i]);
+      }
     }
-  };
+    return result;
+  };};
 });
